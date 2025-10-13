5f27865c360460e7db92b5e4af974173ccf7e7fa
a385868-ctrl <a385868@uach.mx>
2025-10-13 09:52:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ radio = st.slider("Selecciona el radio",0.0, 10.0, 5.0)
 #calculo del area
 area = math.pi * radio **2
 #Mostrar resultado
-st.write(f"El area del circulo {radio} es:
+st.write("El area del circulo {radio} es:
   {area: .2f}")
